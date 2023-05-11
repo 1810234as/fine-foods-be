f9de3aab61383a94baf5c033fe579d40ea1aef95
Sardor <79410721+Alijanov@users.noreply.github.com>
2023-05-11 07:31:18 +0500

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   image: {
     type: String,
-    required: true,
+    required: false,
   },
   name: {
     type: String,
